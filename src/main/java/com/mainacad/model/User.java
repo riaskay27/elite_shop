9a3a21cac63917f359cb09a5a48bda82455a761d
riaskay27 <riaskay2708@ukr.net>
2019-12-11 19:13:58 +0200

    Initial commit

---
+++ src/main/java/com/mainacad/model/User.java
@@ -0,0 +1,30 @@
+package com.mainacad.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	private Integer id;
+	private String login;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+
+	public User(String login, String password, String firstName, String lastName, String email, String phone) {
+		this.login = login;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+	}
+}
